0e83b4041373c5d26aa8ad41a2f208a349cbc601
Micah <micahf1@vt.edu>
2013-06-08 22:08:03 -0400

    Fixing Stock Scaffolding

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,3 +1,7 @@
 class Stock < ActiveRecord::Base
   attr_accessible :name, :price, :symbol
+  validates :name, :price, :symbol, :presence => true
+  validates :name, :price, :uniqueness => true
+
+
 end
